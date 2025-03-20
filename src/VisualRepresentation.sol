04c712b8beaa26bd119c792762b7d5180f46c65c
Andysword <andrewhuang2019@gmail.com>
2025-03-19 20:53:11 -0500

    Updated VisualRepresentation and WalletManager
    Updated both Visual Representation and Wallet Manager contracts to be upgradeable

--- src/VisualRepresentation.sol
+++ src/VisualRepresentation.sol
@@ -3,14 +3,20 @@
 pragma solidity ^0.8.0;
 
 import "lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
 
-contract VisualRepresentation is Initializable {
+// We might want to use a proxy contract and make this upgradeable
+contract VisualRepresentation is Initializable, UUPSUpgradeable, OwnableUpgradeable  {
 
     mapping(uint256 => string) public visuals;
 
     function generateVisual(uint256 claimId, string calldata inputData) public {
-        //reading inputData for the 
+        //reading inputData for the visual.
+        //currently it is a string, but could be turned into however we want it stored.
     }
 
-
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
 }
--- src/WalletManager.sol
+++ src/WalletManager.sol
@@ -3,8 +3,11 @@
 pragma solidity ^0.8.0;
 
 import "lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
 
-contract LinkManager is Initializable {
+// We probably want to use a proxy contract 
+contract WalletManager is Initializable, UUPSUpgradeable, OwnableUpgradeable {
 
     //initializes wallet for the user 
     function createWallet(address user) public {
@@ -15,4 +18,8 @@ contract LinkManager is Initializable {
     function exportWallet(address user) public {
 
     }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
 }
