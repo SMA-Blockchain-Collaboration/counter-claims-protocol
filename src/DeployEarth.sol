9d804403da6c6a24f40506e8cff743aa5ee146bc
Andysword <andrewhuang2019@gmail.com>
2025-06-06 16:02:52 -0500

    Created contracts for Earth ERC-6551

---
+++ src/DeployEarth.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ClaimEarth";
+
+contract DeployEarth {
+  event CreatedEarth(address account, )
+
+  //deploys the single Earth Token, and emits an event on deployment
+  function createEarth(){
+    emit CreatedEarth()
+  }
+
+  //returns the address of the token on the chain
+  function getAddress(){
+
+  }
+}
