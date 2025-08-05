3c9d3c1c59e26b3ec0617cce76a789d215e05257
Andysword <andrewhuang2019@gmail.com>
2025-08-05 17:41:06 -0500

    transformed ClaimLogic to extend ERC 721 standards

--- src/ClaimLogic.sol
+++ src/ClaimLogic.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "lib/openzeppelin-contracts-upgradeable/contracts/token/ERC721/ERC721Upgradeable.sol";
 import "lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 import "lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
 import "lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
 
 import "./DeployEarthWallet.sol";
 
-contract ClaimLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+contract ClaimLogic is Initializable, ERC721Upgradeable, UUPSUpgradeable, OwnableUpgradeable {
     struct Claim {
-        address claimer;
         string title;
         string coordinates;
         string description;
@@ -23,7 +23,7 @@ contract ClaimLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 public earthClaimId;
     address public earthWalletFactory;
     address public earthWalletAddress;
-    bool public isEarthClaimMinted = false;
+    bool public isEarthClaimMinted;
 
     //events that claims have been minted
     event ClaimMinted(
@@ -41,26 +41,25 @@ contract ClaimLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable {
 
     function initialize(address initialOwner, address factoryAddress) public initializer {
         __Ownable_init(initialOwner); // Set the owner
+        __ERC721_init("ClaimToken", "CLM");
         __UUPSUpgradeable_init(); // Initialize UUPSUpgradeable
 
         require(!isEarthClaimMinted, "Earth claim already exists");
-        //if claimId or earthClaim or earthWalletAddress exists, then does not run code below
 
+        //if claimId or earthClaim or earthWalletAddress exists, then does not run code below
         string memory title = "Earth";
         string memory coordinates = "0,0";
         string memory description = "The root claim that represents the Earth";
 
-        //deploy the TBA wallet
-        //address deployedWallet = DeployEarthWallet(factoryAddress).deployEarth(address(this), claimCounter);
-
         //need to set claimer as either a contract, the blockchain, or sma account
         claims[claimCounter] =
             Claim({claimer: initialOwner, title: title, coordinates: coordinates, description: description});
+        _safeMint(initialOowner, claimCounter);
 
         earthClaim = claims[claimCounter];
         earthClaimId = claimCounter;
-
         earthWalletFactory = factoryAddress;
+        isEarthClaimMinted = true;
 
         claimCounter++;
         isEarthClaimMinted = true;
@@ -69,10 +68,7 @@ contract ClaimLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable {
         emit EarthClaimMinted(earthClaimId, initialOwner, title, coordinates, description);
     }
 
-    function linkEarthWallet(address walletAddress) external onlyOwner {
-        require(earthWalletAddress == address(0), "Earth wallet already linked");
-        earthWalletAddress = walletAddress;
-    }
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
     function mintClaim(string memory title, string memory coordinates, string memory description) public {
         require(bytes(title).length > 0, "Title cannot be empty");
@@ -80,7 +76,8 @@ contract ClaimLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable {
         require(bytes(description).length > 0, "Description cannot be empty");
 
         uint256 claimId = claimCounter;
-        claims[claimId] = Claim({claimer: msg.sender, title: title, coordinates: coordinates, description: description});
+        claims[claimId] = Claim({title: title, coordinates: coordinates, description: description});
+        _safeMint(msg.sender, claimId);
 
         emit ClaimMinted(claimId, msg.sender, title, coordinates, description);
         claimCounter++;
@@ -94,18 +91,24 @@ contract ClaimLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable {
         require(bytes(description).length > 0, "Description cannot be empty");
         require(earthWalletAddress != address(0), "Earth wallet not linked yet");
 
-        uint256 claimId = claimCounter;
+        uint256 claimId1 = claimCounter;
+        uint256 claimId2 = claimCounter+1;
 
-        claims[claimId] = Claim({claimer: msg.sender, title: title, coordinates: coordinates, description: description});
+        claims[claimId1] = Claim({title: title, coordinates: coordinates, description: description});
+        claims[claimId2] = Claim({title: title, coordinates: coordinates, description: description});
 
-        //claim that goes to the earth wallet
-        claims[claimId + 1] =
-            Claim({claimer: earthWalletAddress, title: title, coordinates: coordinates, description: description});
+        _safeMint(msg.sender, claimId1);
+        _safeMint(msg.sender, claimId2);
 
         //emit that both claims have been minted
         emit ClaimMinted(claimId, msg.sender, title, coordinates, description);
         emit ClaimMinted(claimId, earthWalletAddress, title, coordinates, description);
         claimCounter += 2;
+    }
+
+    function linkEarthWallet(address walletAddress) external onlyOwner {
+      require(earthWalletAddress == address(0), "Earth wallet already linked");
+      earthWalletAddress = walletAddress;
     }
 
     function ownerOf(uint256 claimId) public view returns (address) {
@@ -114,8 +117,7 @@ contract ClaimLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable {
             //then return the getAddress to find the token's TBA account
             return DeployEarthWallet(earthWalletFactory).getAddress(address(this), claimId);
         }
-        Claim storage claim = claims[claimId];
-        return claim.claimer;
+        return super.ownerOf(claimId);
     }
 
     function getClaimer(uint256 claimId) external view returns (address) {
@@ -127,4 +129,17 @@ contract ClaimLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    function tokenURI (uint256 tokenId) public view override returns (string memory) {
+      require(_exists(tokenId), "Nonexistent token");
+      Claim memory c = claims[tokenId];
+
+      return string(abi.encodePacked(
+          "data:application/json;utf8,{",
+              '"name":"', c.title, '",',
+              '"description":"', c.description, '",',
+              '"attributes":[{"trait_type":"Coordinates","value":"', c.coordinates, '"}]',
+          "}"
+      ));
+    }
 }
